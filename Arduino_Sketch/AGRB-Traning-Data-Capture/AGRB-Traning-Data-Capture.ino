262c1336e2a1e17cd9175a5d25923432fd871f13
ND <ndamen3@users.noreply.github.com>
2020-06-16 12:33:57 -0400

    Docker Addition
    Added A Dockerfile for cleaning and building the model. Added an arduino sketch to collect training data. Added some jupyter notebooks for data cleaning, exploration, and model building.

---
+++ Arduino_Sketch/AGRB-Traning-Data-Capture/AGRB-Traning-Data-Capture.ino
@@ -0,0 +1,18 @@
+
+struct Button {
+  const uint8_t PIN;
+  uint32_t numberKeyPresses;
+  bool pressed;
+};
+
+Button button1 = {33, 0, false};
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
