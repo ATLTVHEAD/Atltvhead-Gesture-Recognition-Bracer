a79705515f59588db542e26f8c59dfc67ad148e1
ND <ndamen3@users.noreply.github.com>
2020-06-17 22:12:53 -0400

    Training Data Script
    The Arduino training data collection file is now pretty much set up to push data over serial. 2 seconds of data read/push.
    
    CAD was updated to fit my arm better.

--- Arduino_Sketch/AGRB-Traning-Data-Capture/AGRB-Traning-Data-Capture.ino
+++ Arduino_Sketch/AGRB-Traning-Data-Capture/AGRB-Traning-Data-Capture.ino
@@ -1,18 +1,173 @@
+//#include <WiFi.h>
+//#include <IRCClient.h>
+#include <Math.h>
+#include <Adafruit_LSM6DSOX.h>
 
-struct Button {
-  const uint8_t PIN;
-  uint32_t numberKeyPresses;
-  bool pressed;
-};
+#include<AceButton.h>
+using namespace ace_button;
 
-Button button1 = {33, 0, false};
+
+const int BUTTON_PIN = 33;
+#ifdef ESP32
+  const int LED_PIN = 13;
+#else
+  const int LED_PIN = LED_BUILTIN;
+#endif
+
+const float gyrocal[3]={0.00904999,-0.01169999,-0.00905}; // rads/s
+const float altgyrocal[3]={0.00904999,-0.01169999,-0.0035};; // rads/s
+const float magcal[3]={1.35999999,-22.63,69.08};  // utesla
+
+const int LED_ON = HIGH;
+const int LED_OFF = LOW;
+AceButton button(BUTTON_PIN);
+void handleEvent(AceButton*, uint8_t, uint8_t);
+
+bool bpress = false;
+bool lpress = false;
+bool dpress = false;
+
+int startTime = 0;
+int currTime = 0;
+
+int id = 0;
+
+// For SPI mode, we need a CS pin
+#define LSM_CS 10
+// For software-SPI mode we need SCK/MOSI/MISO pins
+#define LSM_SCK 13
+#define LSM_MISO 12
+#define LSM_MOSI 11
+
+Adafruit_LSM6DSOX sox;
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+  delay(1000); // some microcontrollers reboot twice
+  Serial.begin(115200);
+  while (! Serial); // Wait until Serial is ready - Leonardo/Micro
+  Serial.println(F("setup(): begin"));
 
+  // initialize built-in LED as an output
+  pinMode(LED_PIN, OUTPUT);
+
+  // Button uses the built-in pull up register.
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  // Configure the ButtonConfig with the event handler, and enable all higher
+  // level events.
+  ButtonConfig* buttonConfig = button.getButtonConfig();
+  buttonConfig->setEventHandler(handleEvent);
+  buttonConfig->setFeature(ButtonConfig::kFeatureClick);
+  buttonConfig->setFeature(ButtonConfig::kFeatureDoubleClick);
+  buttonConfig->setFeature(ButtonConfig::kFeatureLongPress);
+  buttonConfig->setFeature(ButtonConfig::kFeatureRepeatPress);
+  buttonConfig->setFeature(ButtonConfig::kFeatureSuppressClickBeforeDoubleClick);
+
+
+  if (!sox.begin_I2C()) {
+    while (1) {
+      delay(10);
+    }
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t temp;
+  sox.getEvent(&accel, &gyro, &temp);
+  id++;
+  currTime = millis();
+  if(bpress){
+    startTime = currTime;
+    bpress = false;
+    id = 0;
+  }
+  if(currTime-startTime <= 2000){
+
+    // see below for csv data transfer example
+    Serial.print("\t\tTemperature ");
+    Serial.print(temp.temperature);
+    Serial.println(" deg C");
+
+    /* Display the results (acceleration is measured in m/s^2) */
+    Serial.print("\t\tAccel X: ");
+    Serial.print(accel.acceleration.x);
+    Serial.print(" \tY: ");
+    Serial.print(accel.acceleration.y);
+    Serial.print(" \tZ: ");
+    Serial.print(accel.acceleration.z);
+    Serial.println(" m/s^2 ");
+
+    /* Display the results (rotation is measured in rad/s) */
+    Serial.print("\t\tGyro X: ");
+    Serial.print(gyro.gyro.x - gyrocal[0]);
+    Serial.print(" \tY: ");
+    Serial.print(gyro.gyro.y - gyrocal[1]);
+    Serial.print(" \tZ: ");
+    Serial.print(gyro.gyro.z - gyrocal[2]);
+    Serial.println(" radians/s ");
+  
+    Serial.print("\t\tStart Time: ");
+    Serial.print(startTime);
+    Serial.print(" \tCurrent Time: ");
+    Serial.print(currTime);
+    Serial.print(" \tDelta Time: ");
+    Serial.print(currTime-startTime);
+    Serial.println(" milliseconds");
+    
+    Serial.print("\t\tDataID: ");
+    Serial.print(id);
+    Serial.println(" count of data");
+    Serial.println(); 
+
+    /* print the data in CSV format
+      Serial.print(aX, 3);
+      Serial.print(',');
+      Serial.print(aY, 3);
+      Serial.print(',');
+      Serial.print(aZ, 3);
+      Serial.print(',');
+      Serial.print(gX, 3);
+      Serial.print(',');
+      Serial.print(gY, 3);
+      Serial.print(',');
+      Serial.print(gZ, 3);
+      Serial.println();
+     */
+  } 
+  button.check();
+}
+
+
+
+void handleEvent(AceButton* /* button */, uint8_t eventType,
+    uint8_t buttonState) {
+
+  switch (eventType) {
+    case AceButton::kEventPressed:
+      digitalWrite(LED_PIN, LED_ON);
+      bpress = true;
+      //bpress = !bpress;
+      //Serial.println("Press");
+      break;
+    case AceButton::kEventReleased:
+      digitalWrite(LED_PIN, LED_OFF);
+      //Serial.println("Release");
+      break;
+    case AceButton::kEventLongPressed:
+      //Serial.println("long press");
+      lpress = true;
+      break;
+    case AceButton::kEventDoubleClicked:
+      //Serial.println("Double Click");
+      dpress = true;
+      break;
+    //case AceButton::kEventClicked:
+      //Serial.println("single Click");
+      //break;        
+  }
 }
