ba27fb3f6bb9905412e7a9aa6026710ffd803327
ATLTVHEAD <12835768+ATLTVHEAD@users.noreply.github.com>
2021-02-02 17:00:22 -0500

    Update Predict_Gesture.py

--- Python_Scripts/Predict_Gesture.py
+++ Python_Scripts/Predict_Gesture.py
@@ -2,6 +2,7 @@
 # Description: Recieved Data from ESP32 Micro via the AGRB-Training-Data-Capture.ino file, make gesture prediction  
 # Written by: Nate Damen
 # Created on July 13th 2020
+# Updated on JAN 29th 2021
 
 import numpy as np 
 import pandas as pd 
@@ -39,18 +40,16 @@ def get_imu_data():
     line = str(serialport.readline(),'utf-8')
     if not line:
         return None
-    #print(line)
-    #if not "Uni:" in line:
-        #return None
+ 
     vals = line.replace("Uni:", "").strip().split(',')
-    #print(vals)
+ 
     if len(vals) != 7:
         return None
     try:
         vals = [float(i) for i in vals]
     except ValueError:
         return ValueError
-    #print(vals)
+ 
     return vals
 
 # Create Reshape function for each row of the dataset
@@ -58,47 +57,50 @@ def reshape_function(data):
     reshaped_data = tf.reshape(data, [-1, 3, 1])
     return reshaped_data
 
-# header for the incomming data
-header = ["deltaTime","Acc_X","Acc_Y","Acc_Z","Gyro_X","Gyro_Y","Gyro_Z"]
-
-#Create a way to see the length of the data incomming, needs to be 760 points. Used for testing incomming data
-def dataFrameLenTest(data):
-    df=pd.DataFrame(data,columns=header)
-    x=len(df[['Acc_X','Acc_Y','Acc_Z']].to_numpy())
-    print(x)
-    return x
 
 #Create a pipeline to process incomming data for the model to read and handle
 def data_pipeline(data_a):
-    df = pd.DataFrame(data_a, columns = header)
-    temp=df[['Acc_X','Acc_Y','Acc_Z']].to_numpy()
-    tensor_set = tf.data.Dataset.from_tensor_slices(
-        (np.array([temp.tolist()],dtype=np.float64)))
+    tensor_set = tf.data.Dataset.from_tensor_slices((data_a[:,:,1:4]))
     tensor_set_cnn = tensor_set.map(reshape_function)
     tensor_set_cnn = tensor_set_cnn.batch(192)
     return tensor_set_cnn
 
-#define Gestures, current data, temp data holder, a first cylce boolean,
-gest_id = {0:'single_wave', 1:'fist_pump', 2:'random_motion', 3:'speed_mode'}
-data = []
-dataholder=[]
-dataCollecting = False
-gesture = ''
-old_gesture = ''
-
-#flush the serial port
-serialport.flush()
-
-while(1):
-    dataholder = get_imu_data()
-    if dataholder != None:
-        dataCollecting=True
-        data.append(dataholder)
-    if dataholder == None and dataCollecting == True:
-        if len(data) == 760:
+def gesture_Handler(sock, rw, data, dataholder, dataCollecting, gesture, old_gesture):
+    dataholder = np.array(get_imu_data())
+    if dataholder.all() != None:
+        #print(dataholder)
+        dataCollecting = True
+        data[0, rw, :] = dataholder
+        rw += 1
+        if rw > 380:
+            rw = 380
+    if dataholder.all() == None and dataCollecting == True:
+        if rw == 380:
             prediction = np.argmax(model.predict(data_pipeline(data)), axis=1)
             gesture=gest_id[prediction[0]]
-            print(gesture)
-        data = []
+        rw = 0
         dataCollecting = False
-        old_gesture=gesture
+    return rw, gesture, old_gesture, dataCollecting
+
+
+if __name__ == "__main__":
+    #define Gestures, current data, temp data holder, a first cylce boolean,
+    gest_id = {0:'wave_mode', 1:'fist_pump_mode', 2:'random_motion_mode', 3:'speed_mode', 4:'pumped_up_mode'}
+    data = np.zeros(shape=(1,380,7))
+    dataholder = np.zeros(shape=(1,7))
+    row = 0
+    dataCollecting = False
+    gesture = ''
+    old_gesture = ''
+
+    #flush the serial port
+    time.sleep(3)
+    serialport.readline()
+
+    while(1):
+        t=time.time()
+        row, gesture, old_gesture, dataCollecting = gesture_Handler(sock,row,data,dataholder,dataCollecting,gesture,old_gesture)
+        if gesture != old_gesture:
+            print(gesture)    
+            old_gesture=gesture
+        
